106d2109275a17986c589bbd3828e710003cf340
Olivia Hinton and Sky Rousse <olivia@>
2016-07-19 10:27:25 -0700

    add integration spec and route for veiwing a survey

--- app.rb
+++ app.rb
@@ -11,3 +11,15 @@ get('/') do
   @surveys = Survey.all
   erb(:index)
 end
+
+post('/surveys') do
+  name = params.fetch("name")
+  @survey = Survey.create({:name => name})
+  @surveys = Survey.all
+  erb(:index)
+end
+
+get('/surveys/:id') do
+  @survey = Survey.find(params.fetch('id').to_i())
+  erb(:survey)
+end
--- spec/survey_integration_spec.rb
+++ spec/survey_integration_spec.rb
@@ -12,3 +12,21 @@ describe("the index path", {:type => :feature}) do
     expect(page).to have_content("Test survey")
   end
 end
+
+describe("the add a survey path", {:type => :feature}) do
+  it("lets a user add a survey name to the database") do
+    visit('/')
+    fill_in("name", :with => "Test Survey")
+    click_button("Submit")
+    expect(page).to have_content("Test survey")
+  end
+end
+
+describe('the survey details path', {:type => :feature}) do
+  it('lets the user find the info for a survey') do
+    test_survey = Survey.create({:name => "Test Survey"})
+    visit('/')
+    click_link('Test survey')
+    expect(page).to have_content("Test survey")
+  end
+end
